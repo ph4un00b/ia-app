93c205d2451249f649068020f8bba9779c033bd5
m1 <1057021+ph4un00b@users.noreply.github.com>
2024-07-30 16:00:56 -0600

    chore(app): remove window manager deps for desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:window_manager/window_manager.dart';
 import 'screens/root.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-
-  if (Platform.isMacOS || Platform.isWindows) {
-    WidgetsFlutterBinding.ensureInitialized();
-    await windowManager.ensureInitialized();
-
-    WindowOptions windowOptions = const WindowOptions(
-      size: Size(600, 800),
-      center: true,
-      // backgroundColor: Colors.transparent,
-      skipTaskbar: false,
-      titleBarStyle: TitleBarStyle.normal,
-      windowButtonVisibility: true,
-      fullScreen: !true,
-    );
-
-    // await WindowManager.instance.setFullScreen(true);
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();
-      await windowManager.focus();
-      await windowManager.setResizable(true);
-    });
-  }
   runApp(const MyApp());
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,16 +9,12 @@ import audio_session
 import just_audio
 import path_provider_foundation
 import record_darwin
-import screen_retriever
 import shared_preferences_foundation
-import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
-  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
