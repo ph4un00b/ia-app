691c122ab944e4f6de062d4dea9b61caac60149f
m1 <1057021+ph4un00b@users.noreply.github.com>
2024-10-31 18:42:13 -0600

    fix(app/onboarding): do not pass to Active state if user still under Onboarding statet

--- lib/features/core/app_state_observer.dart
+++ lib/features/core/app_state_observer.dart
@@ -1,38 +1,42 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:lola_ai_app/main.dart';
+import 'package:path_provider/path_provider.dart';
 
 enum NavigationAction { push, pop, replace }
 
 class AppStateObserver extends NavigatorObserver {
   @override
-  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+  Future<void> didPush(
+      Route<dynamic> route, Route<dynamic>? previousRoute) async {
     if (route.settings.name == '/voz') {
-      AppStatus.instance.currentStatus = AppState.active;
+      final directory = await getApplicationDocumentsDirectory();
+      final remindersFile = File('${directory.path}/jamon.md');
+
+      AppStatus.instance.currentStatus =
+          remindersFile.existsSync() ? AppState.active : AppState.onboarding;
     }
-    _logNavigation(route, NavigationAction.push);
+    _logNavigation(route: route, action: NavigationAction.push);
   }
 
   @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    _logNavigation(route, NavigationAction.pop);
+    _logNavigation(route: route, action: NavigationAction.pop);
   }
 
   @override
   void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
     if (newRoute != null) {
-      _logNavigation(newRoute, NavigationAction.replace);
+      _logNavigation(route: newRoute, action: NavigationAction.replace);
     }
   }
 
-  void _logNavigation(Route<dynamic> route, NavigationAction action) {
-    final routeName = route.settings.name;
-    if (routeName != null) {
-      log('Screen ${action.name}: $routeName', name: 'Navigation');
-      // print('👻 Screen ${action.name}: $routeName name: Navigation');
-    } else {
-      log('Screen ${action.name}: $routeName', name: 'Navigation');
-    }
-  }
+  void _logNavigation({
+    required Route<dynamic> route,
+    required NavigationAction action,
+  }) =>
+      log('Screen ${action.name}: ${route.settings.name ?? ' 😡 unnamed'}',
+          name: 'Navigation');
 }
