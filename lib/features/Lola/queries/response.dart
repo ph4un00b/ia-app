cb638dddee6320f9185b1eb00c3abd8726feddcc
m1 <1057021+ph4un00b@users.noreply.github.com>
2024-11-26 01:48:15 -0600

    refactor: lola query

--- lib/features/Lola/queries/response.dart
+++ lib/features/Lola/queries/response.dart
@@ -4,7 +4,6 @@ import 'package:lola_ai_app/features/Agents/types.dart';
 import 'package:lola_ai_app/features/App/status.dart';
 import 'package:lola_ai_app/features/Lola/types.dart';
 import 'package:lola_ai_app/features/Reminders/types.dart';
-import 'package:lola_ai_app/features/User/types.dart';
 import 'package:lola_ai_app/services/ReminderAgent/mutations/create_reminder.dart';
 import 'package:lola_ai_app/services/ReminderAgent/mutations/onboarding_reminder.dart';
 import 'package:path/path.dart' as p;
@@ -21,37 +20,34 @@ class LolaResponse {
   }) async {
     final userIntent = await StructuredAgent.classification.query(userQuery);
 
-    if (AppStatus.isCreatingReminder() && userIntent.isConversational) {
-      return AppStatus.isOnboarding()
-          ? await OnboardingReminderHandler.handle(userQuery, voiceModel)
-          : await CreateReminderHandler.handle(userQuery, voiceModel);
-    }
-
-    if (AppStatus.isOnboarding() && userIntent.isConversational) {
+    if (AppStatus.isOnboarding()) {
       const message =
           "Hola! Para poder usar mi inteligencia primero debes crear un recordatorio."
           " Cuando grabes un mensaje puedes mencionar algo como: 'Recuerdame preparar yogurt los sabados por la mañana.'";
 
-      return AppStatus.instance.reminderStatus == ReminderState.idle
-          ? LolaResult(
-              p.normalize((await voiceModel.synthesize(text: message)).path),
-              message,
-            )
-          : await OnboardingReminderHandler.handle(userQuery, voiceModel);
+      return switch ((
+        userIntent.isConversational,
+        AppStatus.instance.reminderStatus == ReminderState.idle
+      )) {
+        (true, true) => LolaResult(
+            p.normalize((await voiceModel.synthesize(text: message)).path),
+            message,
+          ),
+        (true, false) =>
+          await OnboardingReminderHandler.handle(userQuery, voiceModel),
+        (false, true) =>
+          await OnboardingReminderHandler.handle(userQuery, voiceModel),
+        (false, false) =>
+          await OnboardingReminderHandler.handle(userQuery, voiceModel),
+      };
     }
 
     final shouldHandleReminder =
         AppStatus.instance.lolaStatus == LolaState.creatingReminder ||
             userIntent == IntentKind.createReminder;
 
-    return switch (AppStatus.instance.currentUserStatus) {
-      UserState.active => shouldHandleReminder
-          ? CreateReminderHandler.handle(userQuery, voiceModel)
-          : AskLola.query(userQuery, voiceModel, intention: userIntent),
-      UserState.onboarding =>
-        OnboardingReminderHandler.handle(userQuery, voiceModel),
-      UserState.idle || UserState.auth => throw StateError(
-          "${AppStatus.instance.currentUserStatus} state not implmented"),
-    };
+    return shouldHandleReminder
+        ? CreateReminderHandler.handle(userQuery, voiceModel)
+        : AskLola.query(userQuery, voiceModel, intention: userIntent);
   }
 }
