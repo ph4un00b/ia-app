f7cc04208b145cbf871cb05c8dcd0a7fcc3ed14f
m1 <1057021+ph4un00b@users.noreply.github.com>
2025-02-01 23:24:00 -0600

    chore: no resize

--- lib/screens/voz/voz_screen.dart
+++ lib/screens/voz/voz_screen.dart
@@ -28,6 +28,7 @@ class VozScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(
           'Voz',
