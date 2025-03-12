db338df18a60b84f7980c17e6449781e08fdca4c
m1 <1057021+ph4un00b@users.noreply.github.com>
2025-03-11 23:14:07 -0600

    chore

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lola_ai_app/screens/auth/auth_screen.dart';
 import 'package:lola_ai_app/screens/voz/chat_screen.dart';
-import 'package:lola_ai_app/screens/voz/initial_screen.dart';
 
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
